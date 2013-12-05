75c49bdfc88a5ffe8e7f3a752f9ad21b69a85e33
César Alberto <cesaratj27@gmail.com>
2013-12-04 22:19:58 -0600

    Update DDNewton.java

--- DDNewton.java
+++ DDNewton.java
@@ -25,11 +25,16 @@
 
 public class DDNewton{
 	// Propiedades
+	int grado;
+	double []Y;
+	double []X;
+	double [][]diferencias;
+	double punto;
 
 
 	// Constructor
-	public DDNewton(float param){
-
+	public DDNewton(double point){
+		this.punto = point;
 	}
 
 	//Métodos
@@ -39,8 +44,10 @@ public class DDNewton{
 	*	@return: void
 	*	@param: real
 	*/
-	public void setX(float x){
-
+	public void setX(double x[]){
+		grado = x.length;
+		X = new double[grado];
+		System.arraycopy(x, 0, this.X, 0, x.length);
 	}
 
 	/** 
@@ -48,19 +55,42 @@ public class DDNewton{
 	*	@return: void
 	*	@param: real
 	*/
-	public void setY(float y){
-
+	public void setY(double y[]){
+		grado = y.length;
+		Y = new double[grado];
+		System.arraycopy(y, 0, this.Y, 0, y.length);
 	}
 
 	/** 
-	*	Metodo para establecer el polinomio
+	*	Metodo para establecer Y
 	*	@return: void
 	*	@param: real
 	*/
-	public float[] getPolinomio(){
+	public double[] getPolinomio(){
+		int i,j,k;
+		double fx[] = new double[grado];
+		grado = X.length;
+		diferencias = new double[grado-1][grado-1];
 
+		for (i = 0; i < diferencias.length; i++) {
+			diferencias[i][0] = Math.rint(((Y[i+1] - Y[i]) / (X[i+1] - X[i]))*100)/100;
+		}
+		
+		k = 2;
+		for (i = diferencias.length-2; i >= 0; i--) {
+			for (j = 1; j < diferencias.length; j++) {
+				if(j < k){
+					diferencias[i][j] = Math.rint(((diferencias[i+1][j-1] - diferencias[i][j-1]) / (X[i+2] - X[i]))*100)/100;
+				}
+			}
+			k++;
+		}
+
+		fx[0] = Y[0];
+		for (i = 0;i < diferencias.length ; i++) {
+			fx[i+1] = diferencias[0][i];
+		}
+
+		return fx;
 	}
-
-
-
 }
